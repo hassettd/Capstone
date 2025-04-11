90942d369f22028d389a48f9d6bab39dc661e02b
Daniel Hassett <danielhassett@Daniels-Mini.attlocal.net>
2025-04-11 13:33:50 -0400

    fixed syntax for netlify

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,10 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "./app.css"; // Custom styles after Bootstrap import
+import "./App.css"; // Custom styles after Bootstrap import
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 
@@ -17,15 +15,3 @@ root.render(
     <App />
   </Provider>
 );
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   document.getElementById("root")
-// );
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>
-// );
